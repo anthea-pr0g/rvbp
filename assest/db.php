3e7d50f55e99ee3f98a31b4004a892376138fd79
anthea-pr0g <antheayip02@gmail.com>
2022-06-26 17:33:25 +0800

    db pw

--- assest/db.php
+++ assest/db.php
@@ -1,8 +1,15 @@
 <?php
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'blog');
+ 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["b436d63b6a0fa4"];
+$cleardb_password = $cleardb_url["f7cb29fa"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     /* Attempt to connect to MySQL database */
     try{
