0fc5a76e44e8a4a4ed97fbabb4503350bebf3e63
anthea-pr0g <antheayip02@gmail.com>
2022-06-27 12:55:50 +0800

    define

--- assest/db.php
+++ assest/db.php
@@ -8,6 +8,12 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
+
+define('DB_SERVER',$cleardb_server);
+define('DB_USER',$cleardb_username);
+define('DB_PASS',$cleardb_password);
+define('DB_NAME',$db_name);
+
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
